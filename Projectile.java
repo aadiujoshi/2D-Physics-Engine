dcb632efde91443a17807fd6d817a149bbc67da1
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2022-07-24 16:14:57 -0500

    Update Projectile.java

--- Projectile.java
+++ Projectile.java
@@ -4,6 +4,7 @@ public abstract class Projectile
 {
     double x; //(cartesian, NOT graphical coordinates) | also the center of the objects/shape
     double y;
+    double rotation;
 
     int depth;
     int width;
@@ -14,20 +15,34 @@ public abstract class Projectile
 
     InitialPoint initial; //stores inital velocities 
 
+    boolean grounded;
+
+    //only for air resistance
+    // double dragCoefficient;
+    // double accelerationX;
+    // double accelerationY;
+   
     double time;
 
-    public Projectile(double x, double y, int depth, int width, int height, double mass, InitialPoint iPoint)
+    public Projectile(InitialPoint initial, int depth, int width, int height, double volume, double mass, double dragCoefficient)
     {
-        this.x = x;
-        this.y = y;
+        this.x = initial.x;
+        this.y = initial.y;
+        this.grounded = false;
+        // this.accelerationX = 0;
+        // this.accelerationY = 0; //determined by the physicshandler
+        this.rotation = 0;
         this.depth = depth;
         this.width = width;
         this.height = height;
-        this.initial = iPoint;
-        this.volume = depth*width*height;
+        this.volume = volume;
+        // this.dragCoefficient = dragCoefficient;
+        this.initial = initial;
         this.mass = mass;
         this.time = 0;
     }
 
     abstract public void drawObject(int fWidth, int fHeight, int xOffset, Graphics2D g);
+    abstract public double getProjected2DX();
+    abstract public double getProjected2DY();
 }
