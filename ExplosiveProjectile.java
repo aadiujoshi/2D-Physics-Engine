41e3c49cad118659fe076c1e1636923f0da7e0a1
BeanFactory <98367091+aadiujoshi@users.noreply.github.com>
2022-07-24 16:18:24 -0500

    Add files via upload

---
+++ ExplosiveProjectile.java
@@ -0,0 +1,36 @@
+import java.awt.*;
+
+public class ExplosiveProjectile extends Projectile
+{
+    double projRadius;
+    double blastForce;
+    double blastRadius;
+
+    public ExplosiveProjectile(InitialPoint initial, int depth, int width, int height, double mass, double projRadius, double blastRadius, double blastForce)
+    {
+        super(initial, depth, width, height, ((double)4/3)*Math.PI*(Math.pow(projRadius, 3)), mass, 0.47);
+        this.blastRadius = blastRadius;
+        this.projRadius = projRadius;
+    }
+
+    public void drawObject(int fWidth, int fHeight, int xOffset, Graphics2D g) 
+    {
+        g.setColor(Color.GREEN);
+        g.setStroke(new BasicStroke(2));
+
+        int cx = (fWidth/2)+(int)x-xOffset;
+        int cy = fHeight-(int)y-40;
+
+        g.drawOval(cx-(width/2), cy-(height/2), width, height);
+    }   
+
+    public double getProjected2DX() 
+    {
+        return Math.PI*projRadius*projRadius;
+    }
+
+    public double getProjected2DY() 
+    {
+        return Math.PI*projRadius*projRadius;
+    }
+}
